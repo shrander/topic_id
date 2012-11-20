998e4ad796358b6ca884827e34a9cdc030caae4f
Nick <nhnatiw@gmail.com>
2012-11-20 10:58:30 -0500

    Added sql query and search func
    need to add the data into the query from the movie object.  I didn't have schema information.

--- db_pop.py
+++ db_pop.py
@@ -10,8 +10,16 @@ ia = IMDb()
 
 fh = open('movie_list', 'r')
 
-i=0
-while(i<1000):
-	mov_list = ia.search_movie(random.randint(1,1332012))
-	
-	c.execute("INSERT into movie_table VALUES ("+mov['']+") ")
+#i=0
+#while(i<1000):
+	#mov_list = ia.search_movie(random.randint(1,1332012))
+# read in a file of movie titles 
+for each in fh.readlines():
+    # search the movie on IMDB
+    mov_list = ia.search_movie(each)
+    # Enter the first movie found into the DB
+	c.execute("INSERT into movie_table VALUES ("+mov['']+","+mov[]+  ") ")
+    conn.commit()
+    
+#close the connection to the db
+conn.close()
