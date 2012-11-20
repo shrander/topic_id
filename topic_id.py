984926cf76e7c4ef4b4e46b70c773d584c60efa9
Nick <nhnatiw@gmail.com>
2012-11-20 12:56:55 -0500

    Update topic_id.py
    adding classes

--- topic_id.py
+++ topic_id.py
@@ -1,1 +1,26 @@
+import sys
 from imdb import IMDb
+import sqlite3 as sql
+
+class app:
+    def _init_(self):
+      pass
+    
+    def usage(self):
+      print 'USAGE: topic_id.py [options] [synapsis file]\n'
+      print 'This program will determine what genres a movie is in by the contents of the title and synapsis'
+      print 'Options:'
+      print ' -k'
+
+def parse_args(self, args):
+      pass
+    
+    def main(self, args):
+      self.parse_args(args)
+
+
+
+
+if __name__ == '__main__':
+    a = app()
+    a.main(sys.argv)
