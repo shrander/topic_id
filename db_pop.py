a9b5edb4bade5c18a93a1fcb14218da35c4b85f5
shrander <nhnatiw@gmail.com>
2012-11-26 01:47:30 -0500

    added some stuff - not done

--- db_pop.py
+++ db_pop.py
@@ -1,25 +1,60 @@
 import sqlite3
 from imdb import IMDb
 import random
+import sys
 
+if len(sys.argv) != 2:
+    raise Exception('Options Error')
+
+movie_list = sys.argv[1]
 conn = sqlite3.connect('topic.db')
 
 c = conn.cursor()
 
 ia = IMDb()
 
-fh = open('movie_list', 'r')
-
-#i=0
-#while(i<1000):
-	#mov_list = ia.search_movie(random.randint(1,1332012))
-# read in a file of movie titles 
+fh = open(movie_list, 'r')
+ 
 for each in fh.readlines():
+    
     # search the movie on IMDB
     mov_list = ia.search_movie(each)
-    # Enter the first movie found into the DB
-	c.execute("INSERT into movie_table VALUES ("+mov['']+","+mov[]+  ") ")
+    if len(mov_list)>0:
+        query = c.execute('select count(*) from movie_table WHERE title = \"'+mov_list[0]['title']+'\";')
+        if query.fetchone()[0] != 0:
+            print mov_list[0]['title']+': Already in DB'
+            continue
+    else:
+        print each+': not found'
+        continue
+    # get the first result
+    try:
+        mov = ia.get_movie(mov_list[0].getID())
+    except Exception as e:
+        print e
+        print mov_list
+    # get lead actors name !! this will not always be 
+    # the lead actors name as some cast orders are alphabetically ordered
+    try:
+        actor = mov['cast'][0]['name']
+    except KeyError as e:
+        print e
+        print mov['title']
+        #c.execute("INSERT into movie_table VALUES ("+mov.getID()+",\""+mov['title']+'\",\"'+mov['plot'][0]+'\",'+None+","+mov['genre']+")")
+        #c.commit()
+        continue
+    try:
+        # collapse list object to string
+        genre = ''
+        for each in mov['genre']:
+            genre += each+','
+        # Enter the first movie found into the DB
+        c.execute('INSERT INTO movie_table (id,title,plot,lead_actor,genre_list) VALUES ('+mov.getID()+',\"'+mov['title']+'\",\"'+mov['plot'][0]+'\",\"'+actor+'\",\"'+genre[:-1]+'\");')
+    except Exception as e:
+        print e
+        print mov['title']
+        continue
     conn.commit()
-    
+    print 'added '+mov['title']
 #close the connection to the db
 conn.close()
--- topic_id.py
+++ topic_id.py
@@ -2,22 +2,51 @@ import sys
 from imdb import IMDb
 import sqlite3 as sql
 
+class parser:
+    def 
 class app:
-    def _init_(self):
-      pass
-    
+    def __init__(self):
+        self.db_file = ''
+        self.db_hndl = None
+        self.db_connection = None
+        self.db_cursor = None
+        self.synopsis_file = ''
+        self.syn_hndl = None
+        self.training_flag = False
+        
     def usage(self):
-      print 'USAGE: topic_id.py [options] [synapsis file]\n'
-      print 'This program will determine what genres a movie is in by the contents of the title and synapsis'
-      print 'Options:'
-      print ' -k'
+        print 'USAGE: topic_id.py [options] [synapsis file]\n'
+        print 'This program will determine what genres a movie is in by the contents of the title and synapsis'
+        print 'Options:'
+        print ' -k              Not sure if I need this flag'
+        print ' -T [filename]   Sets the training flag.  The file expected is one generated'
+        print '                  by db_pop.py.'
+        print ''
+        sys.exit()
 
-def parse_args(self, args):
-      pass
+    def parse_args(self, args):
+        i=0
+        while(i<len(args)):
+            if args[i] == -T:
+                i+=1
+                self.db_file=args[i]
+            else:
+                #assume the synopsis file is the unknown
+                self.synopsis_file = args[i]
+                self.training_flag = True
+    
+    def init_data(self):
+        try:
+            if self.training_flag:
+                self.db_hndl = open(self.db_file, 'r')
+            else:
+                self.syn_hndl = open(self.synopsis_file, 'r')
+        except IOError e:
+            print e
     
     def main(self, args):
-      self.parse_args(args)
-
+        self.parse_args(args)
+        self.init_data()
 
 
 
